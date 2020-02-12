b4bfe407615fc4644d1779e3cfc4d62c835af306
Ahmed <ahmedealsalman@gmail.com>
2020-02-12 14:21:35 +0200

    task 3 code done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,17 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    "my_list":[{
 
+    "restaurant_name": "Fooders",
+    "food_type": "Edible objects"}
+    ,{"restaurant_name": "27 Brothers",
+    "food_type": "Bakery"}
+    ,{"restaurant_name": "pizzazo",
+    "food_type": "Pizza"}
+    ,
+
+    ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +24,11 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object": {
+    "restaurant_name":"Fooders",
+    "food_type":"Edible objects"
+
+    }
 
     }
     return render(request, 'detail.html', context)
